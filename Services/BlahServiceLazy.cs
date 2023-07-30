9098fd859779d4b8d9914553b0b221e4e0bdddb8
rosdvp <45929573+rosdvp@users.noreply.github.com>
2023-07-30 15:35:38 +0300

    Services. BlahServiceLazy. InitIfNot

--- Services/BlahServiceLazy.cs
+++ Services/BlahServiceLazy.cs
@@ -13,5 +13,7 @@ public class BlahServiceLazy<T> where T: BlahServiceBase
 	private T _service;
 
 	public T Get => _service ??= _context.Get<T>();
+
+	public void InitIfNot() => _context.Get<T>();
 }
 }
